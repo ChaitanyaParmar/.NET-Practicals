0686fcd3ef7d639500bbe6ff024783506d467fda
ChaitanyaParmar <68532604+ChaitanyaParmar@users.noreply.github.com>
2021-01-12 17:41:08 +0530

    Add files via upload

---
+++ Practical_14.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Console
+Module Practical_14
+    Sub Main()
+        Dim a(2, 3) As Integer
+        Dim i, j, val As Integer
+
+        For i = 0 To UBound(a)
+            For j = 0 To UBound(a, 1)
+                Write("Enter value for a({0})({1}): ", i, j)
+                a(i, j) = CInt(ReadLine())
+            Next
+        Next
+
+        Write("Enter value for searching: ")
+        val = CInt(ReadLine())
+
+        For i = 0 To UBound(a)
+            For j = 0 To 2
+                If a(i, j) = val Then
+                    WriteLine("The value found at a({0})({1})", i, j)
+                End If
+            Next
+        Next
+        Read()
+    End Sub
+End Module
---
+++ Practical_15.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Console
+Module Practical_15
+    Sub main()
+        Dim a As Integer()() = New Integer(4)() {}
+        a(0) = New Integer() {1, 5, 7}
+        a(1) = New Integer() {12, 0, 4, 6, 9}
+        a(2) = New Integer() {2}
+        a(3) = New Integer() {1, 1, 4}
+        a(4) = New Integer() {44, 5, 77, 69}
+        Dim b, c As Integer
+
+        For b = 0 To UBound(a)
+            For c = 0 To UBound(a(b))
+                WriteLine("The value on index a({0})({1}) is {2}", b, c, a(b)(c))
+            Next
+        Next
+        Read()
+    End Sub
+End Module
